e84514c23d9e8134afe5664f6ce2e7d82185f303
mocchi <boom.boom.planet@gmail.com>
2015-10-24 21:08:25 +0900

    add reactions

---
+++ reactions.lisp
@@ -0,0 +1,41 @@
+(require 'cl-slack.core "./core.lisp")
+
+(provide 'cl-slack.reactions)
+
+(defpackage :cl-slack.reactions
+  (:use :cl)
+  (:export add get-item fetch-list remove-reaction))
+
+(in-package :cl-slack.reactions)
+
+(defmethod add ((client cl-slack.core:slack-client)
+                (name string)
+                (optionals list))
+  (cl-slack.core:send "reactions.add"
+                      (format nil "?token=~A&name=~A~A"
+                              (cl-slack.core:token client)
+                              name
+                              (cl-slack.core:to-param optionals))))
+
+(defmethod get-item ((client cl-slack.core:slack-client)
+                     (optionals list))
+  (cl-slack.core:send "reactions.add"
+                      (format nil "?token=~A~A"
+                              (cl-slack.core:token client)
+                              (cl-slack.core:to-param optionals))))
+
+(defmethod fetch-list ((client cl-slack.core:slack-client)
+                       (optionals list))
+  (cl-slack.core:send "reactions.list"
+                      (format nil "?token=~A~A"
+                              (cl-slack.core:token client)
+                              (cl-slack.core:to-param optionals))))
+
+(defmethod remove-reaction ((client cl-slack.core:slack-client)
+                            (name string)
+                            (optionals list))
+  (cl-slack.core:send "reactions.remove"
+                      (format nil "?token=~A&name=~A~A"
+                              (cl-slack.core:token client)
+                              name
+                              (cl-slack.core:to-param optionals))))
