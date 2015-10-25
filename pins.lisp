22bcd8d8f9912f2f4b6db69cfc7bfad388b5b1eb
mocchi <boom.boom.planet@gmail.com>
2015-10-25 10:56:05 +0900

    add pins

---
+++ pins.lisp
@@ -0,0 +1,34 @@
+(require 'cl-slack.core "./core.lisp")
+
+(provide 'cl-slack.pins)
+
+(defpackage :cl-slack.pins
+  (:use :cl)
+  (:export add fetch-list remove-pin))
+
+(in-package :cl-slack.pins)
+
+(defmethod add ((client cl-slack.core:slack-client)
+                (channel string)
+                (optionals list))
+  (cl-slack.core:send "pins.add"
+                      (format nil "?token=~A&channel=~A~A"
+                              (cl-slack.core:token client)
+                              channel
+                              (cl-slack.core:to-param optionals))))
+
+(defmethod fetch-list ((client cl-slack.core:slack-client)
+                      (channel string))
+  (cl-slack.core:send "pins.list"
+                      (format nil "?token=~A&channel=~A"
+                              (cl-slack.core:token client)
+                              channel)))
+
+(defmethod remove-pin ((client cl-slack.core:slack-client)
+                       (channel string)
+                       (optionals list))
+  (cl-slack.core:send "pins.remove"
+                      (format nil "?token=~A&channel=~A~A"
+                              (cl-slack.core:token client)
+                              channel
+                              (cl-slack.core:to-param optionals))))
